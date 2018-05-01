05fc25d413578945c0dafc24da415379332d2555
ebiz <lustriaebiz@gmail.com>
2018-05-01 13:24:53 +0700

    note

--- src/Note/Note.jsx
+++ src/Note/Note.jsx
@@ -2,16 +2,26 @@ import React, { Component } from 'react';
 import './Note.css';
 import PropTypes from 'prop-types';
 
-class Note extends Component {
-    constructor(props) {
+class Note extends Component{
+
+    constructor(props){
         super(props);
-        this.noteContent = props.noteContent;
-        this.noteId = props.noteId;
+        this.noteContent = props.noteContent; 
+        this.noteId = props.noteId; 
+        this.handleRemoveNote = this.handleRemoveNote.bind(this);
     }
 
-    render(props){
-        return (
+    handleRemoveNote(id){
+        this.props.removeNote(id);
+    }
+
+    render(){
+        return(
             <div className="note fade-in">
+                <span className="closebtn" 
+                      onClick={() => this.handleRemoveNote(this.noteId)}>
+                      &times;
+                </span>
                 <p className="noteContent">{ this.noteContent }</p>
             </div>
         )
